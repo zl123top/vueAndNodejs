55ef4c94de4813856714b3a9afff2ee9cb792fde
zl <1107108799@qq.com>
2018-05-15 17:49:34 +0800

    tijiaole

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -9,7 +9,7 @@
           @select="handleSelect"
           onselectstart="return false"
           >
-          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-menu-item index="1">处理  中心</el-menu-item>
           <el-submenu index="2">
             <template slot="title">我的工作台</template>
             <el-menu-item index="2-1">选项1</el-menu-item>
